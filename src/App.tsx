9087712f3161e41f6752d63f16a95fe3cfcc6438
Mohamed Hema <mohamedhema2017@gmail.com>
2024-01-25 13:26:51 +0200

    main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import Home from "./pages/home/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/footer/Navbar";
+import Footer from "./components/navbar/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
     },
     {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
+      path: "/login",
+      element: <Login />,
     },
   ]);
 
---
+++ src/components/navbar/Footer.tsx
@@ -0,0 +1,6 @@
+import "./footer.scss";
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
